d16b9625c423cc29791bdf46300c21368fdff385
MTC <odogid101@gmail.com>
2026-05-12 12:56:11 +0100

    Update code

--- configQuiz.js
+++ configQuiz.js
@@ -1,4 +1,4 @@
 
   const IMGBB_API_KEY = "a3cc2562e8cdb004624d45ba0cb2667d";
-const API_URL = "https://your-quiz-api.onrender.com"; // Change this
+const API_URL = "https://cbt-quiz.onrender.com"; // Change this
 
